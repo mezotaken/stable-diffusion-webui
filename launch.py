a88a4725983764c34fa036b86f43849a16008f49
Vladimir Repin <32306715+mezotaken@users.noreply.github.com>
2022-11-14 17:47:48 +0300

    ultimate fix

--- launch.py
+++ launch.py
@@ -247,7 +247,8 @@ def tests(test_dir):
 
     print(f"Launching Web UI in another process for testing with arguments: {' '.join(sys.argv[1:])}")
 
-    proc = subprocess.Popen([sys.executable, *sys.argv])
+    with open('test/stdout.txt', "w", encoding="utf8") as stdout, open('test/stderr.txt', "w", encoding="utf8") as stderr:
+        proc = subprocess.Popen([sys.executable, *sys.argv], stdout=stdout, stderr=stderr)
 
     import test.server_poll
     exitcode = test.server_poll.run_tests(proc, test_dir)
