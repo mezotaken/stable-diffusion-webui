84286226e58b9bad2eaf32a0bc6e4aaad06ee029
Vladimir Repin <32306715+mezotaken@users.noreply.github.com>
2022-11-19 01:54:50 +0300

    try to add coverage

--- test/server_poll.py
+++ test/server_poll.py
@@ -1,4 +1,5 @@
 import unittest
+import coverage
 import requests
 import time
 
@@ -16,8 +17,17 @@ def run_tests(proc, test_dir):
     if proc.poll() is None:
         if test_dir is None:
             test_dir = ""
+
+        cov = Coverage()
+        cov.start()
+
         suite = unittest.TestLoader().discover(test_dir, pattern="*_test.py", top_level_dir="test")
         result = unittest.TextTestRunner(verbosity=2).run(suite)
+
+        cov.stop()
+        with open('coverage_report.xml', 'w') as f:
+            cov.xml_report(outfile=f)
+
         return len(result.failures) + len(result.errors)
     else:
         print("Launch unsuccessful")
